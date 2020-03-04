69bfb16c68ba0005d23db531a620a7a393db936c
Md Naim hossain arif <thk.naim@gmail.com>
2020-03-04 09:50:16 +0100

    modify App.vue

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="hello">
-    
+  <div class="index container">
+      <div class="card" v-for="movie in movies" :key="movie.id">
+        <div class="card-content">
+          <h2 class="indigo-text">{{movie.title}}</h2>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -9,6 +13,11 @@ export default {
   name: 'index',
   data () {
     return {
+      movies: [
+       {title: 'Matrix', slug:'matrix', genre: 'Action', id:'1'}, 
+       {title: 'Avengers', slug:'avengers', genre: 'Action', id:'2'}, 
+       {title: 'Terminator', slug:'terminator', genre: 'Action', id:'1'} 
+      ]
       
     }
   }
@@ -17,18 +26,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.index{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.index h2{
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
