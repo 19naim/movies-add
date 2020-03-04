b5c1b0ebf8dbe76a2cb970a1f91c299dd7ee2e55
Md Naim hossain arif <thk.naim@gmail.com>
2020-03-04 09:42:11 +0100

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,18 @@
 <template>
-    
+   <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+         <router-link to="">
+             <span class="nav-title">Movies</span>
+         </router-link> 
+         <a href="" class="btn-floating btn-large halfway-fab pink">
+            <router-link to="">
+                <i class="material-icons">add</i>
+            </router-link>     
+         </a>
+      </div>      
+    </nav>
+</div> 
 </template>
 
 <script>
